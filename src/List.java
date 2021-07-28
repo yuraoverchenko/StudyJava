61687b483d2be01e43c4dd435a76727e99f8a58d
yuraoverchenko <yuraoverchenkoub@gmail.com>
2021-07-28 17:54:32 +0300

    LinkedList

--- src/List.java
+++ src/List.java
@@ -1,14 +1,27 @@
 public class List {
         Node head = new Node();
 
-         public void add (int value){
+         public void add (int value) {
+
              Node element = new Node(value);
              Node walker = head;
-             while (walker.next != null){
+             while (walker.next != null) {
                  walker = walker.next;
              }
              walker.next = element;
          }
+
+        public Integer getSize(){
+             int size=0;
+            Node walker=head;
+            while(walker.next!=null){
+            walker=walker.next;
+            size++;
+            System.out.println(size);
+        }
+        return size;
+
+    }
          public void print(){
              Node walker = head;
              while (walker.next != null){
