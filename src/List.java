7cee3db62924679453b59443e7baa2f16e14f87c
yuraoverchenko <yuraoverchenkoub@gmail.com>
2021-09-23 17:12:35 +0300

    переробив LinkedList

--- src/List.java
+++ src/List.java
@@ -1,33 +1,34 @@
 public class List {
-        Node head = new Node();
+    Node head = null;
 
-         public void add (int value) {
+    public void add(int value) {
+        if (head == null) {
+            head = new Node(value);
+        } else {
+            Node walker = head;
+            while (walker.next != null) {
+                walker = walker.next;
+            }
+            walker.next = new Node(value);
+        }
+    }
 
-             Node element = new Node(value);
-             Node walker = head;
-             while (walker.next != null) {
-                 walker = walker.next;
-             }
-             walker.next = element;
-         }
-
-        public Integer getSize(){
-             int size=0;
-            Node walker=head;
-            while(walker.next!=null){
-            walker=walker.next;
+    public Integer getSize() {
+        int size = 0;
+        Node walker = head;
+        while (walker != null) {
+            walker = walker.next;
             size++;
             System.out.println(size);
         }
         return size;
-
     }
-         public void print(){
-             Node walker = head;
-             while (walker.next != null){
-                 walker = walker.next;
-                 System.out.println(walker.value);
-             }
-         }
-}
 
+    public void print() {
+        Node walker = head;
+        while (walker != null) {
+            System.out.println(walker.value);
+            walker = walker.next;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         List list = new List();
-
         list.add(14);
         list.add(45);
         list.add(55);
         list.add(85);
         list.print();
+        list.getSize();
     }
 }
